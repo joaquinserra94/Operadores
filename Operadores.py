a80220f16775cdc1e8643bd61e066f0297a5d49d
Joaquin <joaquinserra94@gmail.com>
2024-02-18 11:04:21 +0100

    Programa para evaluar los diferentes operadores a utilizar en python

---
+++ Operadores.py
@@ -0,0 +1,14 @@
+y = 2
+x = 6
+z = 7
+
+print(f"{x} mas {y} es igual a {x+y}")
+print(f"{x} menos {y} es igual a {x-y}")
+print(f"{x} por {y} es igual a {x*y}")
+print(f"{x} dividido {y} es igual a {x/y}")
+
+print(f"{z} dividido al piso {y} es igual a {z//y}") #al piso, quitamos los decimales
+print(f"{z} modulo de {y} es igual a {z%y}") #esto es para obtener el resto
+print(f"{x} elevado a la {y} es igual a {x**y}")
+print(f"{x} elevado a la {3} es igual a {x**3}")
+print(f"La raiz cuadrada de {x} es igua a {x**0.5}")
